85d58d8cb229a1f41d941ea59de1f218a51a4a08
reyhaneh <r.sadatshokouhi@gmail.com>
2021-07-28 18:52:33 +0430

    kitchen collection added

---
+++ app/models/v1/Kitchen.js
@@ -0,0 +1,27 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+const timestamps = require('mongoose-timestamp');
+
+let Kitchen = new Schema({
+    active: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    GPS: { type: Object, required: true,
+        type: {
+          type: String, 
+          enum: ['Point'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },
+    area: { type: String, required: true },
+    distance: { type: Number }
+});
+
+
+Kitchen.plugin(timestamps);
+
+module.exports = mongoose.model('Kitchen', Kitchen);
