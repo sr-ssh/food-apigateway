150583e8666d85c4754f0dda02e7669e61838351
reyhaneh <r.sadatshokouhi@gmail.com>
2021-08-10 11:31:03 +0430

    fix get avtive order

--- app/controllers/kitchen/v1/OrderController.js
+++ app/controllers/kitchen/v1/OrderController.js
@@ -27,7 +27,8 @@ module.exports = new class OrderController extends Controller {
 
             let cookOrder = orders.filter(order => 
                 order.status.status === config.beforeCookOrder && 
-                order.cookId?.toString() === req.decodedData.user_id
+                order.cookId &&
+                order.cookId.toString() === req.decodedData.user_id
                 )
 
             if(!cookOrder.length)
