84c080f9e4faf7cfc0636b4782a56e2d5c93d6b0
saeid niyabaty <saeidniyabaty@gmail.com>
2021-12-09 09:30:38 +0330

    adding editStation

--- app/routes/user/v1/home.js
+++ app/routes/user/v1/home.js
@@ -126,7 +126,9 @@ router.post('/verificationcode', HomeController.verificationCode.bind(HomeContro
  * @apiGroup user
  * @apiSuccessExample {json} Success-Response:
  * {success:true,message:"عملیات با موفقیت انجام شد",data:{}}
- * @apiErrorExample {json} Error-Response:{}
+ * @apiErrorExample {json} Error-Response:
+ * {
+ * }
  */
 router.get('/stations', HomeController.getStations.bind(HomeController));
 
@@ -144,9 +146,33 @@ router.get('/stations', HomeController.getStations.bind(HomeController));
  * @apiParam  {Number} dimeter
  * @apiSuccessExample {json} Success-Response:
  * {success:true,message:"عملیات با موفقیت انجام شد",data:{}}
- * @apiErrorExample {json} Error-Response:{}
+ * @apiErrorExample {json} Error-Response:
+ * {
+ * }
  */
 router.post('/stations', HomeController.addStations.bind(HomeController));
+
+/**
+ * @api {put} /api/user/v1/stations editStations
+ * @apiVersion 1.0.0
+ * @apiName editing stations
+ * @apiDescription editing stations
+ * @apiGroup user
+ * @apiParam  {varchar} _id
+ * @apiParam  {String} description
+ * @apiParam  {boolean} active
+ * @apiParam  {Number} latitude
+ * @apiParam  {Number} longitudes
+ * @apiParam  {Number} code
+ * @apiParam  {Number} dimeter
+ * @apiSuccessExample {json} Success-Response:
+ * {success:true,message:"عملیات با موفقیت انجام شد",data:{}}
+ * @apiErrorExample {json} Error-Response:
+ * {
+ * }
+ */
+router.put('/stations', HomeController.editStations.bind(HomeController));
+
 
 
 module.exports = router;
